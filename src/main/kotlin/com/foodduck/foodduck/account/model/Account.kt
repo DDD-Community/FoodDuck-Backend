f45d30785922480e8de8d39fd7b860dcf4fd8e72
VIXXPARK <vixx170627@gmail.com>
2022-05-25 09:53:02 +0900

    기본 테이블 구성 작성

--- src/main/kotlin/com/foodduck/foodduck/account/model/Account.kt
+++ src/main/kotlin/com/foodduck/foodduck/account/model/Account.kt
@@ -1,4 +1,24 @@
 package com.foodduck.foodduck.account.model
 
-class Account {
+import com.foodduck.foodduck.base.domain.BaseEntity
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name= "ACCOUNT")
+class Account(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ACCOUNT_ID")
+    val id: Long? = null,
+
+    var nickname: String,
+
+    var password: String,
+
+    var lastLogin: LocalDateTime
+
+): BaseEntity() {
+
+
 }
