208999cd152aad025d7e808d23fac0959ffd2313
VIXXPARK <vixx170627@gmail.com>
2022-06-22 09:26:08 +0900

    시큐리티 스웨거 허용

--- src/main/kotlin/com/foodduck/foodduck/base/config/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/foodduck/foodduck/base/config/security/WebSecurityConfig.kt
@@ -6,6 +6,7 @@ import com.foodduck.foodduck.base.config.security.jwt.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -17,13 +18,19 @@ class WebSecurityConfig(
 ) {
 
     @Bean
+    fun webConfigure() = WebSecurityCustomizer { web -> web.ignoring().antMatchers("swagger-ui/index.html", "swagger*/**") }
+
+
+    @Bean
     fun configure(http: HttpSecurity?): SecurityFilterChain? {
         return http?.csrf()?.disable()?.exceptionHandling()?.authenticationEntryPoint(customAuthenticationEntryPoint)?.and()
             ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.and()?.authorizeRequests()
             ?.antMatchers(
                 "/api/v1/accounts/**",
                 "/api/v1/tag-menus/**",
-                "/api/v1/menus/{\\d+}"
+                "/api/v1/menus/{\\d+}",
+                "/swagger*/**",
+                "/v2/api-docs*/**"
             )?.permitAll()
             ?.anyRequest()?.authenticated()?.and()
             ?.addFilterBefore(JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
